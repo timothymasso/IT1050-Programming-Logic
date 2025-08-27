5937d41cb670aa8ec9a3a2a7387641085972d66d
Timothy Masso <timothy.masso@gmail.com>
2025-08-27 19:54:17 -0400

    readme update

--- IT Labs/LabOneContent/LabOne/obj/Debug/net9.0/LabOne.AssemblyInfo.cs
+++ IT Labs/LabOneContent/LabOne/obj/Debug/net9.0/LabOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9ef0d351a82c807027c9b0d58ed1c061f52abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be39777eaec88ac1052266719db6827c4bcdd152")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
