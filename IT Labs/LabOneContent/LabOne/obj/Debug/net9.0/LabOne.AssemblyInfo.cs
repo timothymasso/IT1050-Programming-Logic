e773abbb89ba24307d8573b20d1f5dbafc8ddaa3
Timothy Masso <timothy.masso@gmail.com>
2025-09-14 19:44:58 -0400

    minilab one

--- IT Labs/LabOneContent/LabOne/obj/Debug/net9.0/LabOne.AssemblyInfo.cs
+++ IT Labs/LabOneContent/LabOne/obj/Debug/net9.0/LabOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c999c9df57e729b5720a107d38c2c91f1ed201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d805034c82b78d2275886c007907c160e2e2a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ IT Labs/Mini Exam 1/Mini Exam1/Program.cs
@@ -0,0 +1,49 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            string name;
+            double height;
+            double weight;
+            double bmi = 0;
+
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter height (in inches): ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter weight (in pounds): ");
+            weight = Convert.ToDouble(Console.ReadLine());
+
+            bmi = ((703 * weight) / (height * height));
+
+            Console.WriteLine("\nName: " + name);
+            Console.WriteLine("Height: " + height + " inches");
+            Console.WriteLine("Weight: " + weight + " pounds");
+
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("BMI: " + Math.Round(bmi, 2) + "\nYou are Underweight");
+            }
+            else if (bmi >= 18.5 && bmi <= 24.9)
+            {
+                Console.WriteLine("BMI: " + Math.Round(bmi, 2) + "\nYou have a Normal or Healthy weight");
+            }
+            else if (bmi >= 25 && bmi <= 29.9)
+            {
+                Console.WriteLine("BMI: " + Math.Round(bmi, 2) + "\nYou are Overweight");
+            }
+            else
+            {
+                Console.WriteLine("BMI: " + Math.Round(bmi, 2) + "\nYou are Obese");
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+}
